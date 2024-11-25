ee96bdd2d5f8c56d6fcb68483a547bff5a6865d2
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-25 15:07:46 +0000

    finished Q1

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,10 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
+redo = True
+while redo:
+  word = str(input())
+  if word == "quit":
+    break
+  else:
+    redo = True
+    print(len(word))
