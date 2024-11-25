9d95782942c354232ae5f3650b80eea7613beff7
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-25 15:23:42 +0000

    finsihed q2

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,3 +1,4 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
 word = str(input())
-print(word[-1:len(word)])
+for i in range(len(word)-1, 0-1, -1):
+    print (word[i])
